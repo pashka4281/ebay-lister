59c7568e2f22b6d78386c463408e3fa2e54d01e9
Pavlo Sirous <pashka4281@gmail.com>
2017-05-06 06:36:13 +0300

    add bootstrap via npm

---
+++ client/client.js
@@ -0,0 +1,6 @@
+//import 'tether/dist/js/tether.min.js';
+//import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/alert.js';
+import 'bootstrap/js/dist/button.js';
+import 'bootstrap/js/dist/modal.js';
